9ace18d2fda26b33836d5c864edda5b7d06cb707
sisi <101252799+Amis97@users.noreply.github.com>
2025-07-16 00:58:44 +0330

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(_file_), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_home():
     from app import app
